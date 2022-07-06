626da85d5472d94080921077e1c2a2de2c7b139f
sboyapati <sboyapati@osidigital.com>
2022-07-06 22:51:51 +0530

    feature2 branch

--- LenLastWord.java
+++ LenLastWord.java
@@ -1,9 +1,9 @@
 class Solution {
-    public int lengthOfLastWord(String s) {
-        s=s.trim();
+    public int lengthOfLastWord(String t) {
+        t=s.trim();
         int length=0;
-        for(int i=s.length()-1;i>=0;i--)
-            if(s.charAt(i)!=' ')
+        for(int i=t.length()-1;i>=0;i--)
+            if(t.charAt(i)!=' ')
                 length++;
         else break;
     return length;
